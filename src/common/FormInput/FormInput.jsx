da19dbeecacb26b7471f7c50fe7c351f96dd86f4
Caribla <caribla4@gmail.com>
2024-03-23 12:08:44 +0100

    Update: Register => Adding button to register and setting the user hook and his setState interaction

--- src/common/FormInput/FormInput.jsx
+++ src/common/FormInput/FormInput.jsx
@@ -2,8 +2,8 @@ import "./FormInput.css"
 
 export const FormInput = ({labelText, className, type, name, value, placeholder, onChange, onBlur}) => {
     return(
-        <div className="input-group">
-            <label className="input-label">{labelText}</label>
+        <div className="formInputDesign">
+            <label className="formInputLabel">{labelText}</label>
             <input
                 className={className} 
                 type={type} 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,51 +1,70 @@
-import { FormInput } from "../../common/FormInput/FormInput"
-import { Header } from "../../common/Header/Header"
 import "./Register.css"
+import { useState } from "react"
+import { Header } from "../../common/Header/Header"
+import { FormInput } from "../../common/FormInput/FormInput"
+import { FormButton } from "../../common/FormButton/FormButton"
 
 export const Register = () => {
+    const [user, setUser] = useState({
+        fullname: "",
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    const registerInputHandler = (e) => {
+        setUser((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
+
     return (
         <div className="registerDesign">
             <Header/>
             <div className="registerContent">
                 <FormInput
                     labelText={"fullname"}
-                    className={"input-field"}
+                    className={"formInputField"}
                     type={"text"}
                     name={"fullname"}
                     placeholder={"Write your entire name"}
-                    value={""}
-                    onChange={() => console.log("Hello")}
+                    value={user.fullname || ""}
+                    onChange={e => registerInputHandler(e)}
                     onBlur={() => console.log("World")}
                 />
                 <FormInput
                     labelText={"username"}
-                    className={"input-field"}
+                    className={"formInputField"}
                     type={"text"}
                     name={"username"}
                     placeholder={"Write a username"}
-                    value={""}
-                    onChange={() => console.log("Hello")}
+                    value={user.username || ""}
+                    onChange={e => registerInputHandler(e)}
                     onBlur={() => console.log("World")}
                 />
                 <FormInput
                     labelText={"email"}
-                    className={"input-field"}
+                    className={"formInputField"}
                     type={"email"}
                     name={"email"}
                     placeholder={"Write an email"}
-                    value={""}
-                    onChange={() => console.log("Hello")}
+                    value={user.email || ""}
+                    onChange={e => registerInputHandler(e)}
                     onBlur={() => console.log("World")}
                 />
                 <FormInput
                     labelText={"password"}
-                    className={"input-field"}
+                    className={"formInputField"}
                     type={"password"}
                     name={"password"}
                     placeholder={"Write a password"}
-                    value={""}
-                    onChange={() => console.log("Hello")}
+                    value={user.password || ""}
+                    onChange={e => registerInputHandler(e)}
                     onBlur={() => console.log("World")}
+                />
+                <FormButton
+                    buttonText={"REGISTER"}
                 />
             </div>
         </div>
