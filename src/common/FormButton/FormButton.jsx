ed93faa320700d512847e280adf9a79057580ee9
Caribla <caribla4@gmail.com>
2024-03-23 11:49:10 +0100

    Feat: App => Custom FormButton for all application

---
+++ src/common/FormButton/FormButton.jsx
@@ -0,0 +1,9 @@
+import "./FormButton.css"
+
+export const FormButton = ({buttonText}) => {
+    return (
+        <div className="formButtonDesign">
+            {buttonText}
+        </div>
+    )
+}
