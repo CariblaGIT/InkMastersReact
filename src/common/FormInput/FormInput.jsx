b9415262fd8362f56dfb635276ecf4657f47ce6a
Caribla <caribla4@gmail.com>
2024-03-23 11:22:14 +0100

    Update: RegisterView => Adding and fit inputs on the view

--- src/common/FormInput/FormInput.jsx
+++ src/common/FormInput/FormInput.jsx
@@ -3,9 +3,8 @@ import "./FormInput.css"
 export const FormInput = ({labelText, className, type, name, value, placeholder, onChange, onBlur}) => {
     return(
         <div className="input-group">
-            <label className="input-label" htmlFor="formInput">{labelText}</label>
+            <label className="input-label">{labelText}</label>
             <input
-                id="formInput"
                 className={className} 
                 type={type} 
                 name={name} 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,4 @@
+import { FormInput } from "../../common/FormInput/FormInput"
 import { Header } from "../../common/Header/Header"
 import "./Register.css"
 
@@ -6,7 +7,46 @@ export const Register = () => {
         <div className="registerDesign">
             <Header/>
             <div className="registerContent">
-            Soy la vista de register
+                <FormInput
+                    labelText={"fullname"}
+                    className={"input-field"}
+                    type={"text"}
+                    name={"fullname"}
+                    placeholder={"Write your entire name"}
+                    value={""}
+                    onChange={() => console.log("Hello")}
+                    onBlur={() => console.log("World")}
+                />
+                <FormInput
+                    labelText={"username"}
+                    className={"input-field"}
+                    type={"text"}
+                    name={"username"}
+                    placeholder={"Write a username"}
+                    value={""}
+                    onChange={() => console.log("Hello")}
+                    onBlur={() => console.log("World")}
+                />
+                <FormInput
+                    labelText={"email"}
+                    className={"input-field"}
+                    type={"email"}
+                    name={"email"}
+                    placeholder={"Write an email"}
+                    value={""}
+                    onChange={() => console.log("Hello")}
+                    onBlur={() => console.log("World")}
+                />
+                <FormInput
+                    labelText={"password"}
+                    className={"input-field"}
+                    type={"password"}
+                    name={"password"}
+                    placeholder={"Write a password"}
+                    value={""}
+                    onChange={() => console.log("Hello")}
+                    onBlur={() => console.log("World")}
+                />
             </div>
         </div>
     )
