026a907d27f0b41749fbba518a74b905d678dcc9
Caribla <caribla4@gmail.com>
2024-03-27 21:06:57 +0100

    Feat: Appointments => Default values for dropdowns + All finished for making PostAppointment

--- src/common/FormDropdown/FormDropdown.jsx
+++ src/common/FormDropdown/FormDropdown.jsx
@@ -1,22 +1,25 @@
 import "./FormDropdown.css"
 
-export const FormDropdown = ({array, dataType}) => {
+export const FormDropdown = ({array, dataType, name, labelText, onChange}) => {
     return (
-        <select className="formDropdownDesign">
-            {dataType === "services" ? (
-                array.map((item) => {
-                    return(
-                        <option className="optionDropdown" key={item.id} value={item.serviceName}>{item.serviceName}</option>
-                    )
-                })
-            ) : (
-                array.map((item) => {
-                    return(
-                        <option className="optionDropdown" key={item.username} value={item.fullname}>{item.fullname}</option>
-                    )
-                })
-            )}
-            {}
-        </select>
+        <>
+            <label className="formDropdownLabel">{labelText}</label>
+            <select className="formDropdownDesign" name={name} onChange={onChange} defaultValue="">
+                <option value="" disabled hidden>Choose here</option>
+                {dataType === "services" ? (
+                    array.map((item) => {
+                        return(
+                            <option className="optionDropdown" key={item.id} value={item.serviceName}>{item.serviceName}</option>
+                        )
+                    })
+                ) : (
+                    array.map((item) => {
+                        return(
+                            <option className="optionDropdown" key={item.username} value={item.fullname}>{item.fullname}</option>
+                        )
+                    })
+                )}
+            </select>
+        </>
     )
 }
--- src/common/PopUpAppointment/PopUpAppointment.jsx
+++ src/common/PopUpAppointment/PopUpAppointment.jsx
@@ -4,9 +4,11 @@ import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import { FormInput } from "../FormInput/FormInput"
 import { FormDropdown } from "../FormDropdown/FormDropdown"
+import { FormButton } from "../FormButton/FormButton"
 
 export const PopUpAppointment = (props) => {
     const [actualDate, setActualDate] = useState("");
+    const [disablePost, setDisablePost] = useState("disabled");
 
     const [appointment, setAppointment] = useState({
         date: "",
@@ -18,6 +20,23 @@ export const PopUpAppointment = (props) => {
         const actualDate = getDate()
         setActualDate(actualDate)
     }, [])
+
+    useEffect(() => {
+        if(appointment.date !== "" && 
+        appointment.tattooer !== "" &&
+        appointment.service !== ""){
+            setDisablePost("")
+        } else {
+            setDisablePost("disabled")
+        }
+    }, [appointment])
+
+    const appointmentInputHandler = (e) => {
+        setAppointment((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
 
     const getDate = () => {
         const actualDate = new Date()
@@ -31,6 +50,11 @@ export const PopUpAppointment = (props) => {
             day = '0' + day.toString();
         }
         return year + '-' + month + '-' + day;
+    }
+
+    const saveNewAppointment = () => {
+        console.log("SAVED")
+        props.onHide()
     }
 
     return (
@@ -48,21 +72,31 @@ export const PopUpAppointment = (props) => {
                         type={"date"}
                         name={"date"}
                         min={actualDate}
-                        onChange={e => console.log("Hey")}
-                        onBlur={e => console.log("Hey")}
+                        onChange={e => appointmentInputHandler(e)}
                     />
                     <FormDropdown
                         array={props.services}
                         dataType={"services"}
+                        name={"service"}
+                        labelText={"services"}
+                        onChange={e => appointmentInputHandler(e)}
                     />
                     <FormDropdown
                         array={props.tattooers}
                         dataType={"tattooers"}
+                        name={"tattooer"}
+                        labelText={"tattooers"}
+                        onChange={e => appointmentInputHandler(e)}
                     />
                 </div>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
+                <FormButton
+                    buttonText={"ADD"}
+                    className={"formButtonDesignEdit"}
+                    onClickFunction={saveNewAppointment}
+                    disabled={disablePost}
+                />
             </Modal.Footer>
         </Modal>
     )
